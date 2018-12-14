ade2ccfbbe31a139c54cfc129c1fbc4b641e8d06
jeevan lal <jeevan50070@gmail.com>
2018-12-14 19:46:40 +0530

    Display List done

--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,64 +1,52 @@
 package com.example.android.todolist;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    private static final String  TAG = "MainActivity";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
+        ArrayList<String> task = new ArrayList<>();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.action_add_task:
+        task.add(" Call Dad");
+        task.add("Go to Gym");
+        task.add("Drink Milk");
+/**
+        // giving id to the LinearLayout and finding the LinearLayout so that we add TextViews to that LinearLayout
+       LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-                final EditText editText = new EditText(MainActivity.this);
-                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("add you task here");
-                builder.setTitle("task dialog");
-                builder.setView(editText);
+       for(int i = 0; i < task.size(); i++) {
+           // now lets create a TextView object and add it to the LinearLayout
 
-                // Add the buttons
-                builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
-                        String strValue = editText.getText().toString();
-                        Log.d(TAG,"log here " + strValue);
+           TextView textView = new TextView(this);
 
+           // now we have the TextView object textview, go ahead and add it to the LinearLayout to display the TextVies on the screen
 
-                    }
-                });
-                builder.setNegativeButton("cancel", null);
+           textView.setText(task.get(i));
 
-                builder.create();
-                builder.show();
+           rootView.addView(textView);
 
-                return true;
+           }**/
 
-            default:
-                return super.onOptionsItemSelected(item);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, task );
+
+        ListView listView = (ListView) findViewById(R.id.list_view_id);
+
+        listView.setAdapter(itemsAdapter);
         }
     }
-}
+
